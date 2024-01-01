6b812254813167edb2e00cc0b71b3e4a083077c2
CustomLZH <2357316121@qq.com>
2024-01-02 00:10:02 +0800

    修改pom

--- src/main/java/com/monster/security/MonsterSecurityApplication.java
+++ src/main/java/com/monster/security/MonsterSecurityApplication.java
@@ -2,10 +2,8 @@ package com.monster.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class MonsterSecurityApplication {
 
     public static void main(String[] args) {
